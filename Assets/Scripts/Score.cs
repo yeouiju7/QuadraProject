fa550b6150434968746781035a169a4e90492db0
hye3472 <hye3472@gmail.com>
2025-10-16 15:19:26 +0900

    feat: obs

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int score = 0;
+    public static int bestScore = 0;
+
+
+    void Start()
+    {
+        score = 0;
+    }
+    
+
+    void Update()
+    {
+        GetComponent<Text>().text = score.ToString();
+    }
+}
